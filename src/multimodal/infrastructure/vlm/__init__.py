ee6a1914636690edf1d59ea6e2b4ef5193ad04a4
xuzhiheng215 <3644843628@qq.com>
2026-06-16 19:38:32 +0800

    补充

---
+++ src/multimodal/infrastructure/vlm/__init__.py
@@ -0,0 +1,7 @@
+"""
+VLM Infrastructure
+"""
+
+from .qwen_vl_engine import QwenVLEngine
+
+__all__ = ["QwenVLEngine"]
---
+++ src/multimodal/interface/api/__init__.py
@@ -0,0 +1,7 @@
+"""
+API Interface
+"""
+
+from .routes import router
+
+__all__ = ["router"]
